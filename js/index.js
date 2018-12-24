5a938193427eb6675fb5d425f079ac398036ef88
farrasrayhand <35839373+farrasrayhand@users.noreply.github.com>
2018-12-24 15:07:19 +0800

    Create index.js

---
+++ js/index.js
@@ -0,0 +1,16 @@
+$('.nav__toggle-btn').click(function () {
+    $(this).toggleClass('active');
+    console.log("Clicked menu button");
+    $("#mainListDiv").toggleClass("nav__wrapper--visible");
+    $("#mainListDiv").fadeIn();
+
+});
+
+$(window).scroll(function() {
+            if ($(document).scrollTop() > 50) {
+                $('.nav').addClass('highlight');
+                console.log("OK");
+            } else {
+                $('.nav').removeClass('highlight');
+            }
+        });
